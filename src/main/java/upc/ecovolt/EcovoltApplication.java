a2db532161e0fbd34900103b8532a0d58f88b515
PieroSalcedo <salcedop726mail.com>
2026-05-04 14:32:49 -0500

    chore: initial spring boot project structure

---
+++ src/main/java/upc/ecovolt/EcovoltApplication.java
@@ -0,0 +1,13 @@
+package upc.ecovolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcovoltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcovoltApplication.class, args);
+	}
+
+}
