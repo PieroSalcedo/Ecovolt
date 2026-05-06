ee3e38a701c58d4af8b3eaccbc80b3d1ae41330a
PieroSalcedo <salcedop726mail.com>
2026-05-06 02:07:40 -0500

    feat: implementation HomeDto using Mapper

---
+++ src/main/java/upc/ecovolt/mapping/dto/homeDto/HomeMapper.java
@@ -0,0 +1,19 @@
+package upc.ecovolt.mapping.dto.homeDto;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import upc.ecovolt.entity.Home;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface HomeMapper {
+
+    @Mapping(target = "userId", source = "user.id")
+    HomeResponseDto toResponseDto(Home entity);
+
+    @Mapping(target = "user.id", source = "userId")
+    Home toEntity(HomeRequestDto requestDto);
+
+    List<HomeResponseDto> toResponseDtoList(List<Home> entityList);
+}
