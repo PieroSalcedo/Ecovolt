e25555aeeb9e7a2949e4e389f8ad0700225adcf6
PieroSalcedo <salcedop726mail.com>
2026-05-06 02:09:37 -0500

    feat: implementation DeviceDto using Mapper

---
+++ src/main/java/upc/ecovolt/mapping/dto/deviceDto/DeviceRequestDto.java
@@ -0,0 +1,27 @@
+package upc.ecovolt.mapping.dto.deviceDto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class DeviceRequestDto {
+    @NotBlank(message = "El número de serie es obligatorio")
+    @Size(max = 100)
+    private String serialNumber;
+
+    @NotBlank(message = "El nombre del dispositivo es obligatorio")
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank(message = "La categoría es obligatoria")
+    private String category; // Ej: "Climatización"
+
+    private String manufacturer;
+
+    private String firmwareVersion;
+
+    @NotNull(message = "El ID del ambiente (Room) es obligatorio")
+    private Long roomId;
+}
